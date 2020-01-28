5f4f4f121c63ca11e5fe0672ea525d22edd6535a
최정균 <wjdrbs966@naver.com>
2020-01-28 19:06:58 +0900

    다익스트라 알고리즘 공부중

---
+++ Dijkstra/DijkstraTest.java
@@ -0,0 +1,81 @@
+package Algorithm.Dijkstra;
+
+import java.util.PriorityQueue;
+
+class Element implements Comparable<Element> {
+    int index;
+    int distance;
+
+    public Element(int index, int distance) {
+        this.index = index;
+        this.distance = distance;
+    }
+
+    @Override
+    public int compareTo(Element o) {
+        return this.distance - o.distance;
+    }
+}
+
+public class DijkstraTest {
+    static boolean[] visited;
+    static int[] dist;
+    static int[][] ad;
+    static int E = 9, V = 6;
+    static int inf = 100000;
+
+    public static void DijkstaMethod(int start) {
+        PriorityQueue<Element> pq = new PriorityQueue<>();
+        dist[start] = 0;
+        pq.offer(new Element(start, dist[start]));
+
+        while(!pq.isEmpty()) {
+            int cost = pq.peek().distance;
+            int here = pq.peek().index;
+            pq.poll();
+
+            if (cost > dist[here]) {   // 현재 배열에 있는 값(최단 거리)보다 cost(거리)가 더 길면 continue
+                continue;
+            }
+
+            System.out.print(here + " ");
+
+            for (int i = 1; i <= V; ++i) {
+                if (ad[here][i] != 0 && dist[i] > (dist[here] + ad[here][i])) {
+                    dist[i] = dist[here] + ad[here][i];
+                    pq.offer(new Element(i, dist[i]));
+                }
+            }
+
+            System.out.println();
+
+            for (int i = 1; i <= V; ++i) {
+                System.out.print(dist[i] + " ");
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        visited = new boolean[V + 1];
+        dist = new int[V + 1];
+        ad = new int[V + 1][V + 1];
+
+        for (int i = 0; i <= V; ++i) {
+            dist[i] = inf;
+        }
+
+        ad[1][2] = 8;
+        ad[1][3] = 1;
+        ad[1][4] = 2;
+        ad[3][4] = 2;
+        ad[3][2] = 5;
+        ad[4][5] = 3;
+        ad[4][6] = 5;
+        ad[5][6] = 1;
+        ad[6][1] = 5;
+
+        DijkstaMethod(1);
+
+    }
+}
